1735bb863a6bfa1baa5a57f97a297e11044b813b
ariele-fatima <ariele_afd@yahoo.com.br>
2022-04-23 22:31:44 -0300

    Corrigindo tamanho maximo da descricao

--- CleanArchMvc/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -16,7 +16,7 @@ namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
             builder.HasKey(t => t.Id);
 
             builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
-            builder.Property(p => p.Description).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
 
             builder.Property(p => p.Price).HasPrecision(10,2);
 
